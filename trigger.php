b79e75a16b11899d76cd6c15f78facb110794167
css123001 <75751694+css123001@users.noreply.github.com>
2020-12-17 14:34:57 +0000

    Update trigger.php

--- trigger.php
+++ trigger.php
@@ -2,6 +2,6 @@
 curl -X POST \
      -F token=TOKEN \
      -F "ref=REF_NAME" \
-     -F "variables[MYREPONAMESHuser3]=SHuser3" \
-     https://codeingress-test.hic.dundee.ac.uk/api/v4/projects/17/trigger/pipeline
+     -F "variables[MYREPONAME]=SHuser3" \
+     "https://codeingress-test.hic.dundee.ac.uk/api/v4/projects/17/trigger/pipeline"
 ?>
