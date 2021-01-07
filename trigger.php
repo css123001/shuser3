1f61ea6044dea898ae8c4ceba66124e9735bce2f
csword001 <csword001@dundee.ac.uk>
2021-01-07 16:44:22 +0000

    removed test

--- trigger.php
+++
@@ -1,7 +0,0 @@
-<?php
-curl -X POST \
-     -F token=TOKEN \
-     -F "ref=REF_NAME" \
-     -F "variables[MYREPONAME]=SHuser3" \
-     "https://codeingress-test.hic.dundee.ac.uk/api/v4/projects/17/trigger/pipeline"
-?>
